e3a11bbd0f66b23014dc096ae6c24ebee1269d36
Rishabh Shukla <shuklarishabh487@gmail.com>
2026-03-07 00:33:05 -0600

    chore: Setup express server for invoice app.

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import chalk from "chalk"
+import cookieParser from "cookie-parser"
+import "dotenv/config"
+import express from "express"
+import morgan from "morgan"
+
+const app = express()
+
+if (process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+} 
+
+app.use(express.json())
+// INFO: extended: false means we can't pass nested obj.
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+
+app.get("/api/v1/test", (req, res)=>{
+    res.json({
+        Hi: "Hey, Welcome to the Invoice Web App"
+    })
+})
+
+const PORT = process.env.PORT || 1997
+
+app.listen(PORT, ()=>{
+    console.log(
+        `${chalk.green.bold("✅")} 🚀 Server running in ${chalk.greenBright.bold(process.env.NODE_ENV)} mode on ${chalk.blueBright.bold(process.env.PORT)}`
+    )
+})
